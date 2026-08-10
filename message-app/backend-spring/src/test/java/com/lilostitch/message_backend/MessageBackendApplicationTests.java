4df8dfef316737f9e634232166c3ec9e70a0ea4d
sheronjay <192441684+sheronjay@users.noreply.github.com>
2026-08-10 20:58:06 +0530

    chore: initialize spring

---
+++ message-app/backend-spring/src/test/java/com/lilostitch/message_backend/MessageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilostitch.message_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
